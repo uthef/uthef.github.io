a38b26596deea2db80d5be885660ecb25bc96457
Nightcrawler <denbrylski@gmail.com>
2023-07-14 18:36:19 +0300

    console.log => console.info

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
 let api = new UthefAPI();
 
 api.fetch().then(function(data) {
-    console.log("API Python version: " + data["python_version"]);
-    console.log("Current date: " + data["date"]);
+    console.info("API Python version: " + data["python_version"]);
+    console.info("Current date: " + data["date"]);
 }).catch(function(reason) {
     console.error("Unable to reach API");
     console.error(reason);
