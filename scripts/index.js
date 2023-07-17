19135d3040b9261c8b8db27683b97a8ecef44bed
Nightcrawler <denbrylski@gmail.com>
2023-07-18 00:38:53 +0300

    console output changed

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,10 @@
 let api = new UthefAPI();
 
 api.fetch().then(function(data) {
-    console.info("API Python version: " + data["python_version"]);
-    console.info("Current date: " + data["date"]);
+    let output = "API Python version: " + data["python_version"] + "\n";
+    output += "Current date: " + data["date"] + "\n";
+    output += "Request IP: " + data["request_ip"];
+    console.info(output);
 }).catch(function(reason) {
     console.error("Unable to reach API");
     console.error(reason);
