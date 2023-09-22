ad92ea9cba6e77a917507b0468abc6706a543445
Nightcrawler <denbrylski@gmail.com>
2023-09-22 18:27:10 +0300

    index.js updated

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,7 @@ api.fetch().then(function(data) {
     let output = "API Python version: " + data["python_version"] + "\n";
     output += "Current date: " + data["date"] + "\n";
     output += "Request IP: " + data["request_ip"];
+    output += "UTC time: " + data["utc_time"];
     console.info(output);
 }).catch(function(reason) {
     console.error("Unable to reach API");
