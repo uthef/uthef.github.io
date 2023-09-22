cee22618770fddeae973e2bbaefa77327b31bc13
Nightcrawler <denbrylski@gmail.com>
2023-09-22 18:29:10 +0300

    index.js updated

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@ let api = new UthefAPI();
 api.fetch().then(function(data) {
     let output = "API Python version: " + data["python_version"] + "\n";
     output += "Current date: " + data["date"] + "\n";
-    output += "Request IP: " + data["request_ip"];
+    output += "Request IP: " + data["request_ip"] + "\n";
     output += "UTC time: " + data["utc_time"];
     console.info(output);
 }).catch(function(reason) {
