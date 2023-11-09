fdd2bc9d1e76fc3a2825fe390eb8b72c1feebc66
Nightcrawler <denbrylski@gmail.com>
2023-11-09 06:15:20 +0300

    api updated

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,11 @@
 let api = new UthefAPI();
 
 api.fetch().then(function(data) {
-    let output = "API Python version: " + data["python_version"] + "\n";
-    output += "Current date: " + data["date"] + "\n";
+    let output = "Node version: " + data["node_version"] + "\n";
+    let dateUtc = new Date(Date.parse(data["date_utc"]));
+    output += "Current date: " + dateUtc.toLocaleDateString() + "\n";
     output += "Request IP: " + data["request_ip"] + "\n";
-    output += "UTC time: " + data["utc_time"];
+    output += "Local time: " + dateUtc.toTimeString();
     console.info(output);
 }).catch(function(reason) {
     console.error("Unable to reach API");
